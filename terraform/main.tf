eb0ddd04b7df89daa3d9ab3ccbeff52cc913a317
Marco Illescas <mu.illescasflores@ugto.mx>
2026-04-15 19:42:46 -0600

    Update terraform/main.tf (Fixing lambdas flow)

--- terraform/main.tf
+++ terraform/main.tf
@@ -161,6 +161,27 @@ resource "aws_iam_role_policy" "step_function_policy" {
     })
 }
 
+#                       Trigger role                     #
+resource "aws_iam_role" "trigger_role" {
+    name                = "trigger-role"
+    assume_role_policy  = data.aws_iam_policy_document.trigger_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy" "policy_trigger" {
+    name   = "trigger-policy"
+    role   = aws_iam_role.trigger_role.id
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = ["states:StartExecution"]
+                Effect = "Allow"
+                Resource = aws_sfn_state_machine.video_pipeline_sfn.arn
+            }
+        ]
+    })
+}
+
 # ------------------------------------------------------ #
 #                 Packing Lambda functions               #
 # ------------------------------------------------------ #
@@ -180,6 +201,12 @@ data "archive_file" "zip_lambda_metadata" {
     type        = "zip"
     source_dir  = "../src/lambda_metadata"
     output_path = "lambda_metadata.zip"
+}
+
+data "archive_file" "zip_lambda_trigger" {
+    type        = "zip"
+    source_dir  = "../src/lambda_trigger"
+    output_path = "lambda_trigger.zip"
 }
 
 # ------------------------------------------------------ #
@@ -238,6 +265,41 @@ resource "aws_lambda_function" "lambda_metadata" {
             AWS_ENDPOINT_URL = "http://localhost:4566"
         }
     }
+}
+
+resource "aws_lambda_function" "lambda_trigger" {
+    filename         = data.archive_file.zip_lambda_trigger.output_path
+    source_code_hash = data.archive_file.zip_lambda_trigger.output_base64sha256
+    function_name    = "trigger-pipeline"
+    role             = aws_iam_role.trigger_role.arn
+    handler          = "app.handler"
+    runtime          = "python3.9"
+
+    environment {
+        variables = {
+            STATE_MACHINE_ARN = aws_sfn_state_machine.video_pipeline_sfn.arn
+            AWS_ENDPOINT_URL = "http://localhost:4566"
+        }
+    }
+}
+
+resource "aws_lambda_permission" "allow_s3" {
+    statement_id  = "AllowS3Invoke"
+    action        = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.lambda_trigger.function_name
+    principal     = "s3.amazonaws.com"
+    source_arn    = aws_s3_bucket.input_videos_1080p.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+    bucket = aws_s3_bucket.input_videos_1080p.id
+
+    lambda_function {
+        lambda_function_arn = aws_lambda_function.lambda_trigger.arn
+        events              = ["s3:ObjectCreated:*"]
+    }
+
+    depends_on = [aws_lambda_permission.allow_s3]
 }
 
 # ------------------------------------------------------ #
